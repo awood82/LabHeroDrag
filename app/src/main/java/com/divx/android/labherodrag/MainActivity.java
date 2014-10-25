ff2dad64f0c05826468e6530a548b7aa730ce9f8
Andrew Wood <awood82@gmail.com>
2014-10-25 12:43:08 -0700

    Start with the basic layout and images

--- app/src/main/java/com/divx/android/labherodrag/MainActivity.java
+++ app/src/main/java/com/divx/android/labherodrag/MainActivity.java
@@ -2,35 +2,47 @@ package com.divx.android.labherodrag;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
+
+    private static final String KEY = "image";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        // Set up data for drawables in ListView
+        int[] images = new int[] {
+                R.drawable.other1,
+                R.drawable.other2,
+                R.drawable.other3,
+                R.drawable.other1,
+                R.drawable.other2,
+                R.drawable.other3 };
+        List<HashMap<String, String>> listDetails = new ArrayList<HashMap<String, String>>();
+        for (int i = 0; i < images.length; i++) {
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put(KEY, String.valueOf(images[i]));
+            listDetails.add(map);
         }
-        return super.onOptionsItemSelected(item);
+
+        // Set up the ListView
+        ListView listView = (ListView) findViewById(R.id.listOfImages);
+        SimpleAdapter simpleAdapter = new SimpleAdapter(
+                this,
+                listDetails,
+                R.layout.listview_contents,
+                new String[] { KEY },
+                new int[] { R.id.listImageView });
+        listView.setAdapter(simpleAdapter);
+
     }
 }
